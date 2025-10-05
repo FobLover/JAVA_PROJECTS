7ccf8ee0506d248dfb8038c56b26b5d4fa1e7f19
FobLover <sharshil297@gmail.com>
2025-10-06 00:13:03 +0530

    This is my first spring project

---
+++ springbootfirst/src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+
+	public void code() {
+		// TODO Auto-generated method stub
+		System.out.println("We are aliens!");
+	}
+
+}
